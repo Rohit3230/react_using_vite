97351798ae7844f5f3aec2e6fb81fcc161ae8507
Rohit3230 <rsn3230@gmail.com>
2025-03-06 20:01:46 +0530

    fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,14 @@
-import React from 'react'; 
+import React from 'react';
 import AppHeader from "./AppHeader";
-const Home = () => { return (<>        <AppHeader />            <div>                Home Page            </div>        </>); };
+const Home = () => {
+    return (
+        <>
+            <AppHeader />
+            <div>
+               <img src={'https://media.licdn.com/dms/image/v2/C5603AQFMbrOA_Ju7tQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1516962978187?e=2147483647&v=beta&t=tr1TlMF9vUjtDPbMjUgjP7oHKgygJt4Ir6ndtFmey18'}>
+               </img>
+            </div>
+        </>
+    );
+};
 export default Home;
